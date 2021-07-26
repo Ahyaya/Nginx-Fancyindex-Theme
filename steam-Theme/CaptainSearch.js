1c871902f386a11e61c81bcb34b3828c8b3dca43
Chan <40376613+Ahyaya@users.noreply.github.com>
2021-07-26 09:37:20 +0800

    add Chinese characters search

--- steam-Theme/CaptainSearch.js
+++ steam-Theme/CaptainSearch.js
@@ -1,5 +1,5 @@
 /*
-*	JavaScript search bar develop by Captain
+*	JavaScript search bar develop by Captain version 1.01
 *	My website https://arxiv.cloud/
 */
 var form = document.createElement('form');
@@ -15,13 +15,19 @@ document.querySelector('h1').after(form);
 
 var indexContent=document.querySelectorAll("#list tbody tr");
 
+document.querySelectorAll("#list tbody tr a").forEach(
+	function(item){
+		item.id=decodeURI(item.href.substring(item.href.lastIndexOf('\/')+1,item.href.length).toUpperCase());
+	}
+);
+
 input.addEventListener('input', function () {
 	if(this.value==""){
 		indexContent.forEach(function(item) {item.hidden=false;});
 		return;
 	}
 	indexContent.forEach(function(item){item.hidden=true;});
-	var inputword='#list tr td a[href*='+'\''+this.value+'\''+']';
+	var inputword='#list tr td a[id*='+'\''+this.value.toUpperCase()+'\''+']';
 	var myElements = document.querySelectorAll(inputword);
 	myElements.forEach(function(item) {item.parentNode.parentNode.hidden=false;});
 });
